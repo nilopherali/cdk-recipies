13e0dff43004f51e985cdb755e35439918c49bbd
Your Name <you@example.com>
2024-04-21 16:36:05 +0530

    cdk-recipe-project

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <ul>
+      <li>
+        <a class="active" href="#home">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#news">Recipes</a>
+      </li>
+      <li>
+        <a href="#contact">Settings</a>
+      </li>
+    </ul>
+  );
+}
